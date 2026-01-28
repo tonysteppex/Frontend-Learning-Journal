901e73a15646f036808b0987486f6cf2cfc4192f
Tony Stephen <stephentony77@gmail.com>
2026-01-28 17:35:19 +0100

    Smooth scrolling and browser compartabilies

--- Foodies Project/JS/script.js
+++ Foodies Project/JS/script.js
@@ -1,17 +1,65 @@
 console.log("Hello world!");
 
-//Set current year
-const yearEl = document.querySelector(".year");
+// 1. Set current year (Make sure this matches the class in your HTML footer!)
+const yearEl = document.querySelector(".year-number");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-//Make mobile navigation work
+// 2. Make mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    //Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+//Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  },
+);
+obs.observe(sectionHeroEl);
 
 //Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
