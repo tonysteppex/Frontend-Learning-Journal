e37ee4918dca337d47781b1e65cb4b9e3ffd2d5d
Tony Stephen <stephentony77@gmail.com>
2026-06-22 18:40:13 +0100

    Added Elements, Conditions, and dice functionalities

--- JS/Pig Game/script.js
+++ JS/Pig Game/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `Images/dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        // Switch to next player
+        activePlayer = 1;
+    }
+
+});
